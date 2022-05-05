f0f73b4af3cb801f9a0859f498e90a1dbb60859a
Navannetha <nmogulla@unomaha.edu>
2022-05-04 23:49:29 -0500

    Commiting automation scripts

---
+++ account/TestCases/Automation/DownloadReport.py
@@ -0,0 +1,42 @@
+import time
+import unittest
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class ll_ATS(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_ll(self):
+        user = "instructor"
+        pwd = "gounomavs1a"
+        quant = 3
+        delivered = 3
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("http://foodpantry.pythonanywhere.com/")
+        time.sleep(1)
+        driver.find_element_by_xpath("//*[@id=\"id_username\"]").send_keys(user)
+        time.sleep(1)
+        driver.find_element_by_xpath("//*[@id=\"id_password\"]").send_keys(pwd)
+        time.sleep(1)
+        driver.find_element_by_xpath("/html/body/div[2]/form/p[3]/input").click()
+        time.sleep(1)
+        # assert "Logged in"
+        driver.find_element_by_xpath("/html/body/div[1]/ul/li[3]/a").click()
+        time.sleep(1)
+        driver.find_element_by_xpath("/html/body/div[2]/table/tbody/tr[1]/td[8]/a").click()
+        time.sleep(5)
+
+
+def tearDown(self):
+    self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
